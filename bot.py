6cddf58da678d9f7edda8990c5db61f892a9aaa4
bahezhaje904-crypto <bahezhaje904@gmail.com>
2026-05-24 10:36:18 +0300

    youtube android client fix

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 import re
 import glob
 import yt_dlp
+import imageio_ffmpeg
 
 from telegram import Update
 from telegram.ext import (
@@ -44,18 +45,24 @@ async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     os.makedirs("downloads", exist_ok=True)
 
-    ydl_opts = {
-        "format": "bestaudio[ext=m4a]/bestaudio[ext=webm]/bestaudio/best",
-        "outtmpl": "downloads/%(id)s.%(ext)s",
-        "quiet": True,
-        "noplaylist": True,
-        "cookiefile": "cookies.txt",
-        "postprocessors": [{
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": "mp3",
-            "preferredquality": "192",
-        }],
-    }
+   ydl_opts = {
+    "format": "140/251/250/bestaudio",
+    "outtmpl": "downloads/%(id)s.%(ext)s",
+    "quiet": True,
+    "noplaylist": True,
+    "cookiefile": "cookies.txt" if os.path.exists("cookies.txt") else None,
+    "ffmpeg_location": imageio_ffmpeg.get_ffmpeg_exe(),
+    "extractor_args": {
+        "youtube": {
+            "player_client": ["android"]
+        }
+    },
+    "postprocessors": [{
+        "key": "FFmpegExtractAudio",
+        "preferredcodec": "mp3",
+        "preferredquality": "192",
+    }],
+}
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
